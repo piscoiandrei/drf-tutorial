3503b71834a0068f46ac20d3118b047fccf2da69
Piscoi Andrei <piscoiandrei27@gmail.com>
2021-12-07 20:49:43 +0200

    DRF docs tutorial 1

--- drf_tutorial/urls.py
+++ drf_tutorial/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls',
-                              namespace='rest_framework'))
+                              namespace='rest_framework')),
+    path('snippets/', include('snippets.urls')),
 ]
